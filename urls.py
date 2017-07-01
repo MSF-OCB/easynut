e560c09dc4a9e29acf10716376b164d6f47ce390
Doudou <edouard.lavaud@gmail.com>
2017-07-01 16:14:56 +0200

    Corrected textarea in edit page

--- urls.py
+++ urls.py
@@ -1,20 +1,20 @@
 from django.conf.urls import include, url
 from . import views
 
-from rest_framework import routers
+#from rest_framework import routers
 from django.contrib.auth import views as auth_views
 
-from REST import TableViewSet
+#from REST import TableViewSet
 
 app_name = 'emr'
 
-router = routers.DefaultRouter()
-router.register(r'', TableViewSet, base_name='api')
+#router = routers.DefaultRouter()
+#router.register(r'', TableViewSet, base_name='api')
 
 urlpatterns = [
 	url(r'^loginview/$', views.loginview, name='loginview'),
-    url(r'api/', include(router.urls)),
-    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),	
+    #url(r'api/', include(router.urls)),
+    #url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),	
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<record_id>[0-9]+)/patient/$', views.patient, name='patient'),	
 	url(r'^(?P<table_id>[0-9]+)/(?P<record_id>[0-9]+)/detail/$', views.detail, name='detail'),	
