15ca45d4e30a4c7477365cce301d9c5e24bdbcec
RdN-MSF <ramses.de.norre@brussels.msf.org>
2017-07-01 20:09:25 +0200

    Update the REST API.

--- REST.py
+++ REST.py
@@ -13,7 +13,7 @@ from rest_framework import serializers, viewsets
 class Table(object):
     def __init__(self, table_config, **kwargs):
         # For every field in the table, we inject a property in this instance
-        for field in (map(lambda c: c.field_id, table_config.fields)):
+        for field in (['_id'] + map(lambda c: c.field_id, table_config.fields)):
             setattr(self, field, kwargs.get(field, None))
 
 
@@ -32,6 +32,7 @@ class TableSerializer(serializers.Serializer):
             fields = table_config.printable_fields()
 
         # Map the different column types to the correct serializer.
+        self.fields['_id'] = serializers.IntegerField(default=None)
         for column in fields:
             if column.type == FieldConfig.field_type_int:
                 self.fields[self.sanitize(column.name)] = serializers.IntegerField(source=column.field, default=None)
@@ -48,107 +49,17 @@ class TableSerializer(serializers.Serializer):
         table = Table(id=None, table_config=self.table_config, **validated_data)
         to_add = []
         for field_def in self.table_config.fields:
-            if field_def.field_id in validated_data:# and validated_data[field_def.field_id] is not None:
+            if field_def.field_id in validated_data:
                 to_add.append([field_def.field_id, validated_data[field_def.field_id], field_def.type])
-        self.daoobject.insertrecord(self.table_config.id, to_add)
+        id = self.daoobject.insertrecord(self.table_config.id, to_add)
+        table._id = id
         return table
 
     def update(self, instance, validated_data):
-        for field, value in validated_data.items():
-            setattr(instance, field, value)
-        return instance
-
-
-
-#
-# /api/
-#
-# /api?tableid=x  =>  list content of tabla_x
-#
-#
-#mixins.CreateModelMixin,
-class TableViewSet(viewsets.ViewSet):
-    # Required for the Browsable API renderer to have a nice form.
-    serializer_class = TableSerializer
-
-    def __init__(self, **kwargs):
-        super(TableViewSet, self).__init__(**kwargs)
-        self.daoobject = DAO.DAO()
-        self.daoobject.set_tables_config()
-        self.daoobject.set_tables_relationships()
-
-    def list(self, request):
-        #print request.user.is_authenticated
-        if "tableid" not in request.query_params:
-            raise APIException(detail='tableid required')
-
-        tableid = request.query_params["tableid"]
-
-        for tablec in self.daoobject.tables_config:
-            if tablec.id == tableid:
-                where_params = self.extract_where_params(tablec, request.query_params)
-                where_string = ' and '.join(['%s = %s' % (key, value) for (key, value) in where_params.items()])
-
-                print where_string
-
-                c = self.daoobject.db.cursor()
-                query = "select {} from {}"
-                params = (TableViewSet.select_string(tablec), tablec.sql_table_config_name)
-                c.execute(query.format(*params))
-                rows = c.fetchall()
-                fields = map(lambda x: x[0], c.description)
-                results = [dict(zip(fields, row)) for row in rows]
-                records = map(lambda r: Table(tablec, **r), results)
-                c.close()
-
-                serializer = TableSerializer(table_config=tablec, instance=records, many=True, showall=self.only_viewable(request))
-                return Response(serializer.data)
-
-        raise Http404
-
-    def retrieve(self, request, pk=None):
-        #print request.user.is_authenticated
-
-        if pk is None:
-            raise APIException(detail='pk required!')
-        elif "tableid" not in request.query_params:
-            raise APIException(detail='tableid required')
-
-        tableid = request.query_params["tableid"]
-
-        for tablec in self.daoobject.tables_config:
-            if tablec.id == tableid:
-                c = self.daoobject.db.cursor()
-                query = "select {} from {} where _id = {}"
-                params = (TableViewSet.select_string(tablec), tablec.sql_table_config_name, pk)
-                c.execute(query.format(*params))
-                row = c.fetchone()
-                fields = map(lambda x: x[0], c.description)
-                result = dict(zip(fields, row))
-                c.close()
-
-                serializer = TableSerializer(table_config=tablec, instance=Table(tablec, **result), many=False, showall=self.only_viewable(request))
-                return Response(serializer.data)
-        return Http404
-
-    @staticmethod
-    def select_string(table_config):
-        return ', '.join(map(lambda f: f.field, table_config.printable_fields()))
-
-    @staticmethod
-    def only_viewable(request):
-        if "showall" in request.query_params and request.query_params["showall"] == "true":
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def extract_where_params(table_config, query_params):
-        where_params = dict()
-        field_names = map(lambda f: TableSerializer.sanitize(f.name), table_config.fields)
-
-        for (key, value) in query_params.items():
-            if key in field_names:
-                where_params[key] = value
-
-        return where_params
+        print(validated_data)
+        to_edit = []
+        for field_def in self.table_config.fields:
+            if field_def.field_id in validated_data and validated_data[field_def.field_id] is not None and validated_data[field_def.field_id] != '':
+                to_edit.append([field_def.field_id, validated_data[field_def.field_id], field_def.type])
+        self.daoobject.editrecord(self.table_config.id, instance._id, to_edit)
+        return Table(self.table_config, **self.daoobject.select_from_record_id(self.table_config.id, instance._id))
--- urls.py
+++ urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import include, url
 from . import views
 
-from RESTViews import TableList
+from RESTViews import TableList, TableDetail
 
 app_name = 'emr'
 
 urlpatterns = [
 	url(r'^login/$', views.loginview, name='login'),
 	url(r'^logoutbutton/$', views.logoutbutton, name='logoutbutton'),	
-    url(r'api2/', TableList.as_view()),
-    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/(?P<table_id>[0-9]+)/$', TableList.as_view()),
+    url(r'^api/(?P<table_id>[0-9]+)/(?P<pk>(NULL|[0-9]+))/$', TableDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<record_id>[0-9]+)/patient/$', views.patient, name='patient'),	
 	url(r'^(?P<table_id>[0-9]+)/(?P<record_id>[0-9]+)/detail/$', views.detail, name='detail'),	
