d5a25f344612f2b7f249c5588d248885dff7bbc3
RdN-MSF <ramses.de.norre@brussels.msf.org>
2017-07-01 18:27:26 +0200

    Deal with null in addResults view.

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
 from . import views
 
-from django.contrib.auth import views as auth_views
-
 from RESTViews import TableList
 
 app_name = 'emr'
@@ -16,7 +14,7 @@ urlpatterns = [
 	url(r'^(?P<record_id>[0-9]+)/patient/$', views.patient, name='patient'),	
 	url(r'^(?P<table_id>[0-9]+)/(?P<record_id>[0-9]+)/detail/$', views.detail, name='detail'),	
 	url(r'^(?P<table_id>[0-9]+)/(?P<record_id>[0-9]+)/edit/$', views.edit, name='edit'),
-	url(r'^(?P<table_id>[0-9]+)/(?P<related_record_entry>[0-9]+)/(?P<related_record_field>[a-zA-Z_0-9]+)/addrecord/$', views.addrecord, name='addrecord'),		
+	url(r'^(?P<table_id>[0-9]+)/(?P<related_record_entry>(NULL|[0-9]+))/(?P<related_record_field>[a-zA-Z_0-9]+)/addrecord/$', views.addrecord, name='addrecord'),
 	url(r'^results/$', views.results, name='results'),
 	url(r'^save/$', views.save, name='save'),
 	url(r'^(?P<table_id>[0-9]+)/(?P<record_id>[0-9]+)/deleterecord/$', views.deleterecord, name='deleterecord'),	
