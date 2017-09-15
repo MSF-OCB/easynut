32ab344cb9d68fbd33b4fcda8c39802fb97bb1e5
RdN-MSF <ramses.de.norre@brussels.msf.org>
2017-09-15 15:07:51 +0200

    Better path resolution.

--- django_easynut/wsgi.py
+++ django_easynut/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emr.settings")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_easynut.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_easynut.settings")
 
 application = get_wsgi_application()
+
