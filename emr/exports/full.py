765dc288432b77a37e4521195733f27632df84b1
Pascal Polleunus <ppo@especific.be>
2018-01-10 16:21:06 +0100

    Add non dynamic fields in Excel full export.

--- emr/exports/full.py
+++ emr/exports/full.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from openpyxl import Workbook
 
-from ..models import DynamicRegistry
+from ..models import NON_DYNAMIC_FIELDS_DB_COL_NAMES, DynamicRegistry
 
 from .base import AbstractExportExcel
 
@@ -10,11 +10,13 @@ class ExportExcelFull(AbstractExportExcel):
     """Create an Excel file containing an export of the whole database."""
 
     DEFAULT_FILENAME = "easynut-full-export.xlsx"  # Default name for the file.
+    VERBOSE = True  # Whether to include advanced information.
 
     def generate(self):
         """Generate the export."""
         self.book = Workbook()
         DynamicRegistry.load_models_config()
+        verbose_style = self.get_style(self.STYLE_VERBOSE)
 
         # Loop over all models and fields config.
         for model_id, model_config in DynamicRegistry.models_config.iteritems():
@@ -28,6 +30,12 @@ class ExportExcelFull(AbstractExportExcel):
                 sheet.cell(column=col, row=row).value = field_config.name
                 field_ids.append(field_id)
 
+            if self.VERBOSE:
+                col += 1  # Skip a column.
+                for db_col_name in NON_DYNAMIC_FIELDS_DB_COL_NAMES:
+                    col += 1
+                    sheet.cell(column=col, row=row).value = db_col_name
+
             # Apply heading style and freeze heading row.
             style = self.get_style(self.STYLE_HEADING)
             max_col = max(len(model_config.fields_config), self.APPLY_STYLE_MIN_NUM_COLS)
@@ -35,12 +43,25 @@ class ExportExcelFull(AbstractExportExcel):
             sheet.freeze_panes = sheet.cell(column=1, row=row + 1)
 
             # Write values.
-            row = 1
+            min_row = 2  # Skip heading row.
+            row = min_row - 1    # To start the loops with the increment, easier to read.
             for model in model_config.objects.all():
                 col = 0; row += 1  # NOQA
                 for field_id in field_ids:
                     col += 1
                     self.set_cell_value(sheet.cell(column=col, row=row), model.get_field_value(field_id))
+
+                if self.VERBOSE:
+                    col += 1  # Skip a column.
+                    for db_col_name in NON_DYNAMIC_FIELDS_DB_COL_NAMES:
+                        col += 1
+                        self.set_cell_value(sheet.cell(column=col, row=row), getattr(model, db_col_name))
+
+            # Apply verbose style.
+            if self.VERBOSE:
+                verbose_min_col = len(field_ids) + 1
+                verbose_max_col = verbose_min_col + 1 + len(NON_DYNAMIC_FIELDS_DB_COL_NAMES)
+                self.apply_style(verbose_style, sheet, verbose_max_col, row, min_col=verbose_min_col, min_row=min_row)
 
         # Remove the first sheet.
         self.book.remove_sheet(self.book.active)
