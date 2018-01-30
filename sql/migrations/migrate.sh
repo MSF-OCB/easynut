8e633da48e21c04be843bf7fcb756804049e0e38
Pascal Polleunus <ppo@especific.be>
2018-01-30 19:38:11 +0100

    Bug fix: SQL migrations script.

--- sql/migrations/migrate.sh
+++ sql/migrations/migrate.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-filter="${1}*.sql"
+BASE_DIR="$(cd "$(dirname "${0}")" && pwd)"
+
+filter="${BASE_DIR}/${1}*.sql"
 
 files=`ls $filter 2>/dev/null`
 [ -n "$files" ] || { echo "No migrations found."; exit 1; }
 
-for f in $files; do
-  if [ "`mysql -u root -p$MYSQL_ROOT_PASSWORD -D easynutdata -B -N -e "SELECT COUNT(*) FROM sql_migrations WHERE name='${f%.*}'" 2>&1`" != "1" ]; then
-    echo "Applying $f..."
-    mysql -u root -p$MYSQL_ROOT_PASSWORD < "$f"
+for file in $files; do
+  filename=`basename $file`
+  name="${filename%.*}"
+  if [ "`mysql -u root -p$MYSQL_ROOT_PASSWORD -D easynutdata -B -N -e "SELECT COUNT(*) FROM sql_migrations WHERE name='${name}'" 2>&1`" != "1" ]; then
+    echo "Applying $name..."
+    mysql -u root -p$MYSQL_ROOT_PASSWORD < "$file"
   fi
 done
