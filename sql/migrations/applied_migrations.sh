22b43c775cd24bc4673c39cdaa786b4750f4060f
Pascal Polleunus <ppo@especific.be>
2018-01-30 14:56:51 +0100

    Review SQL migrations.
    To work in Bourne shell.
    To use MYSQL_ROOT_PASSWORD.

--- sql/migrations/applied_migrations.sh
+++ sql/migrations/applied_migrations.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-LIMIT=""
-[[ -n $1 ]] && LIMIT="LIMIT $1"
+[ -n "$1" ] && LIMIT="LIMIT $1" || LIMIT=""
 
-mysql -u root -D easynutdata -e "SELECT * FROM sql_migrations ORDER BY applied DESC, _id DESC $LIMIT"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -D easynutdata -e "SELECT * FROM sql_migrations ORDER BY applied DESC, _id DESC $LIMIT"
--- sql/migrations/migrate.sh
+++ sql/migrations/migrate.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 filter="${1}*.sql"
 
 files=`ls $filter 2>/dev/null`
-[[ -n $files ]] || { echo "No migrations found."; exit 1; }
+[ -n "$files" ] || { echo "No migrations found."; exit 1; }
 
 for f in $files; do
-  if [ "`mysql -u root -D easynutdata -B -N -e "SELECT COUNT(*) FROM sql_migrations WHERE name='${f%.*}'" 2>&1`" != "1" ]; then
+  if [ "`mysql -u root -p$MYSQL_ROOT_PASSWORD -D easynutdata -B -N -e "SELECT COUNT(*) FROM sql_migrations WHERE name='${f%.*}'" 2>&1`" != "1" ]; then
     echo "Applying $f..."
-    mysql -u root < "$f"
+    mysql -u root -p$MYSQL_ROOT_PASSWORD < "$f"
   fi
 done
